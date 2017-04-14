edd707a86d426f6c97aadd445b1fc7f4fd71a1fe
ks.lee <ks.lee@ysmcorp.com>
2017-04-14 11:46:06 +0900

    express-handlebars templeate 연동

--- meadowlark.js
+++ meadowlark.js
@@ -2,29 +2,32 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express-handlebars')
+    .create({defaultLayout : 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('Meadwlark Travel');
+  res.render('home');
 });
 
 app.get('/about', function(req, res) {
-  res.type('text/plain');
-  res.send('About Meadwlark Travel');
+  res.render('about');
 });
 
 // 커스텀 404 페이지
 app.use(function(req, res) {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 // 커스텀 500 페이지
 app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 })
 
 // Application listen
