2233a28e364e9d900405c870e672f36699fcb5a0
ks.lee <ks.lee@ysmcorp.com>
2017-04-14 14:09:47 +0900

    static 경로 추가 및 dynamic contents - fortune 추가

--- meadowlark.js
+++ meadowlark.js
@@ -5,6 +5,14 @@ var app = express();
 var handlebars = require('express-handlebars')
     .create({defaultLayout : 'main'});
 
+var fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need spring.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple."
+]
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
@@ -14,8 +22,12 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-  res.render('about');
+  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune:randomFortune });
 });
+
+// static 파일
+app.use(express.static(__dirname + '/public'));
 
 // 커스텀 404 페이지
 app.use(function(req, res) {
