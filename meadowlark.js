e5de0387d543f94e435a6b7b8b166b03b0872bc1
ks.lee <ks.lee@ysmcorp.com>
2017-04-14 14:19:42 +0900

    meadowlark.js 체이닝 메서드 방식으로 변경

--- meadowlark.js
+++ meadowlark.js
@@ -1,8 +1,6 @@
-var express = require('express');
-
-var app = express();
-
-var handlebars = require('express-handlebars')
+var express = require('express'),
+  app = express(),
+  handlebars = require('express-handlebars')
     .create({defaultLayout : 'main'});
 
 var fortunes = [
@@ -13,34 +11,32 @@ var fortunes = [
   "Whenever possible, keep it simple."
 ]
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-app.set('port', process.env.PORT || 3000);
+app
+  .use(express.static(__dirname + '/public'))
+  .set('port', process.env.PORT || 3000);
+  .engine('handlebars', handlebars.engine);
+  .set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-  res.render('home');
-});
-
-app.get('/about', function(req, res) {
-  var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune:randomFortune });
-});
-
-// static 파일
-app.use(express.static(__dirname + '/public'));
+app
+  .get('/', function(req, res) {
+    res.render('home');
+  })
+  .get('/about', function(req, res) {
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune:randomFortune });
+  });
 
 // 커스텀 404 페이지
-app.use(function(req, res) {
-  res.status(404);
-  res.render('404');
-});
-
-// 커스텀 500 페이지
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
-})
+app.
+  use(function(req, res) {
+    res.status(404);
+    res.render('404');
+  })
+  .use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+  })
 
 // Application listen
 app.listen(app.get('port'), function() {
